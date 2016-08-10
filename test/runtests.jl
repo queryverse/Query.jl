6b9f1fb52658705b1f228af84cc5e2547dc2e593
David Anthoff <anthoff@berkeley.edu>
2016-08-10 16:00:57 -0700

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using LINQ
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# For now just make sure all the examples run without errors
+include("../example/01-DataFrame.jl")
+include("../example/02-Dict.jl")
+include("../example/03-Array.jl")
+include("../example/04-SQLite.jl")
+include("../example/05-Nullable.jl")
+include("../example/06-Generator.jl")
+include("../example/07-typedtables.jl")
+include("../example/08-join.jl")
+include("../example/performance.jl")
