446ff0239afd906ce43c0d7932c5c91e4f4ab60d
David Anthoff <anthoff@berkeley.edu>
2016-08-10 22:12:06 -0700

    Update example

--- example/01-DataFrame.jl
+++ example/01-DataFrame.jl
@@ -10,8 +10,12 @@ push!(columns, [23., 42., 59.])
 push!(columns, [3,5,2])
 df = DataFrame(columns, [:name, :age, :children])
 
+# Note that we are using a local julia variable that
+# has the same name as the column in the DataFrame
+children = 2
+
 x = @from i in df begin
-    @where i.age>30. && i.children >2
+    @where i.age>30. && i.children > children
     @select @NT(Name=>lowercase(i.name))
 end collect(DataFrame)
 
