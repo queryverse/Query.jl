4c3e4ea3411852ebc47cdde18f3ebb867cb7f594
David Anthoff <anthoff@berkeley.edu>
2016-08-12 11:38:21 -0700

    Disable SQLite test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Base.Test
 include("../example/01-DataFrame.jl")
 include("../example/02-Dict.jl")
 include("../example/03-Array.jl")
-include("../example/04-SQLite.jl")
+#include("../example/04-SQLite.jl")
 include("../example/05-Nullable.jl")
 include("../example/06-Generator.jl")
 include("../example/07-typedtables.jl")
