b96fd06fb3d4aecd7c3dca73630b7fcd402a65fb
David Anthoff <anthoff@berkeley.edu>
2016-08-16 13:28:28 -0700

    Add examples back to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -150,3 +150,16 @@ end
 @test q==["john", "sally", "kirk"]
 
 end
+
+@testset "Examples" begin
+    include("../example/01-DataFrame.jl")
+    include("../example/02-Dict.jl")
+    include("../example/03-Array.jl")
+    #include("../example/04-SQLite.jl")
+    include("../example/05-Nullable.jl")
+    include("../example/06-Generator.jl")
+    include("../example/07-typedtables.jl")
+    include("../example/08-join.jl")
+    include("../example/09-let.jl")
+    include("../example/10-orderby.jl")
+end
