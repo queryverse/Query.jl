d38b0b57655d4ab680cdff8e91d4571c0ecc792d
David Anthoff <anthoff@berkeley.edu>
2016-08-22 09:57:25 -0700

    Switch to native HTML Documenter output

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,18 @@ using NamedTuples
 using TypedTables
 
 makedocs(
-	modules = [Query]
+	modules = [Query],
+	format = Documenter.Formats.HTML,
+	sitename = "Query.jl",
+	pages = [
+		"Home" => "index.md",
+		"Tutorial" => "tutorial.md",
+		"Internals" => "internals.md"]
 )
 
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+    deps = nothing,
+    make = nothing,
     repo = "github.com/davidanthoff/Query.jl.git",
     julia = "0.5"
 )
