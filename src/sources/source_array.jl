ded0a1068b6f2e999e0a75ce96afceb39e74f8bb
David Anthoff <anthoff@berkeley.edu>
2016-09-05 16:01:43 -0700

    Make array source type more specific

--- src/sources/source_array.jl
+++ src/sources/source_array.jl
@@ -3,8 +3,8 @@ immutable EnumerableArray{T,S} <: Enumerable{T}
     source::S
 end
 
-function query{TS}(source::Array{TS})
-    return EnumerableArray{TS,Array{TS}}(source)
+function query{TS,N}(source::Array{TS,N})
+    return EnumerableArray{TS,Array{TS,N}}(source)
 end
 
 function start{T,S}(iter::EnumerableArray{T,S})
