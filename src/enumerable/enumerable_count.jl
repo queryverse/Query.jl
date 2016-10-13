2f3400e6e66138080675ced85177955ed3cc7b11
David Anthoff <anthoff@berkeley.edu>
2016-10-12 21:39:34 -0700

    Fix type instability in @count

--- src/enumerable/enumerable_count.jl
+++ src/enumerable/enumerable_count.jl
@@ -1,23 +1,34 @@
-function count{T}(source::Enumerable{T}, filter::Function, filter_expr::Expr)
+function _count{T}(source::Enumerable{T}, filter::Function, state)
     count_val = 0
-    state = start(source)
     while !done(source, state)
-        value, state = next(source, state)
-        if filter(value)
+        ret_val = next(source, state)
+        state = ret_val[2]
+        if filter(ret_val[1])
             count_val += 1
         end
     end
     return count_val
 end
 
-function count(source::Enumerable)
+function count{T}(source::Enumerable{T}, filter::Function, filter_expr::Expr)
     count_val = 0
     state = start(source)
+    return _count(source, filter, state)
+end
+
+function _count(source::Enumerable, state)
+    count_val = 0
     while !done(source, state)
-        value, state = next(source, state)
+        ret_val = next(source, state)
+        state = ret_val[2]
         count_val += 1
     end
     return count_val
+end
+
+function count(source::Enumerable)
+    state = start(source)
+    return _count(source, state)
 end
 
 macro count_internal(source, f)
