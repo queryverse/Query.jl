b4253e88a9802efb63615898e285a6bab3dcc80f
David Anthoff <anthoff@berkeley.edu>
2017-03-18 22:16:32 -0700

    Update traits system for julia 0.6

--- src/traits.jl
+++ src/traits.jl
@@ -13,5 +13,16 @@ end
 @traitdef HasCustomTableIterator{X}
 
 @generated function SimpleTraits.trait{X}(::Type{IsIterableTable{X}})
-    istrait(IsTypedIterable{X}) && ( ( Base.iteratoreltype(X)==Base.HasEltype() && eltype(X)<: NamedTuple ) || istrait(HasCustomTableIterator{X}) ) ? :(IsIterableTable{X}) : :(Not{IsIterableTable{X}})
+    if istrait(IsTypedIterable{X})
+        if istrait(HasCustomTableIterator{X})
+            return :(IsIterableTable{X})
+        elseif Base.iteratoreltype(X)==Base.HasEltype()
+            if Base.eltype(X)<: NamedTuple
+                return :(IsIterableTable{X})
+            elseif Base.eltype(X) == Any
+                return :(Base.eltype(X) <: NamedTuples.NamedTuple ? IsIterableTable{X} : Not{IsIterableTable{X}})
+            end
+        end
+    end
+    return :(Not{IsIterableTable{X}})
 end
