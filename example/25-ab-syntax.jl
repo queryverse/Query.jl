30d640aa7596a6e8c34a11437123a9dff6673a8c
florian oswald <florian.oswald@gmail.com>
2017-06-23 23:39:55 +0200

    added example and test for a..b

---
+++ example/25-ab-syntax.jl
@@ -0,0 +1,14 @@
+using Query
+using DataFrames
+
+df = DataFrame(name=repeat(["John", "Sally", "Kirk"],inner=[1],outer=[2]), 
+     age=vcat([10., 20., 30.],[10., 20., 30.].+3), 
+     children=repeat([3,2,2],inner=[1],outer=[2]),state=[:a,:a,:a,:b,:b,:b])
+
+x = @from i in df begin
+    @group i by i.state into g
+    @select {group=g.key,mage=mean(g..age), oldest=maximum(g..age), youngest=minimum(g..age)}
+    @collect DataFrame
+end
+
+println(x)
