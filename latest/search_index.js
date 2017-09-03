2afe12be16f397256c275110f543bd3570b9afff
autodocs <autodocs>
2017-09-03 05:14:45 +0000

    build based on db808be

--- latest/search_index.js
+++ latest/search_index.js
@@ -529,6 +529,78 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "experimental.html#",
+    "page": "Experimental Features",
+    "title": "Experimental Features",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "experimental.html#Experimental-features-1",
+    "page": "Experimental Features",
+    "title": "Experimental features",
+    "category": "section",
+    "text": "The following features are experimental, i.e. they might change significantly in the future. You are advised to only use them if you are prepared to deal with significant changes to these features in future versions of Query.jl. At the same time any feedback on these features would be especially welcome.The @select, @where, @groupby and @orderby (and various variants) commands can be used in standalone versions. Those standalone versions are especially convenient in combination with the pipe syntax in julia. Here is an example that demonstrates their use:using Query, DataFrames\n\ndf = DataFrame(a=[1,1,2,3], b=[4,5,6, 8])\n\ndf2 = df |>\n    @groupby(_.a) |>\n    @select({a=_.key, b=mean(_..b)}) |>\n    @where(_.b > 5) |>\n    @orderby_descending(_.b) |>\n    DataFrameThis example makes use of three experimental features: 1) the standalone query commands, 2) the .. syntax and 3) the _ anonymous function syntax."
+},
+
+{
+    "location": "experimental.html#Standalone-query-operators-1",
+    "page": "Experimental Features",
+    "title": "Standalone query operators",
+    "category": "section",
+    "text": "All standalone query commands can either take a source as their first argument, or one can pipe the source into the command, as in the above example. For example, one can either writedf = df |> @groupby(_.a)ordf = @groupbe(df, _.a)both forms are equivalent.The remaining arguments of each query demand are command specific.The following discussion will present each command in the version that accepts a source as the first argument."
+},
+
+{
+    "location": "experimental.html#The-@select-command-1",
+    "page": "Experimental Features",
+    "title": "The @select command",
+    "category": "section",
+    "text": "The @select command has the form @select(source, element_selector). source can be any source that can be queried. element_selector must be an anonymous function that accepts one element of the element type of the source and applies some transformation to this single element."
+},
+
+{
+    "location": "experimental.html#The-@where-command-1",
+    "page": "Experimental Features",
+    "title": "The @where command",
+    "category": "section",
+    "text": "The @where command has the form @where(source, filter_condition). source can be any source that can be queried. filter_condition must be an anonymous function that accepts one element of the element type of the source and returns true if that element should be retained, and false if that element should be filtered out."
+},
+
+{
+    "location": "experimental.html#The-@groupby-command-1",
+    "page": "Experimental Features",
+    "title": "The @groupby command",
+    "category": "section",
+    "text": "There are two versions of the @groupby command. The simple version has the form @groupby(source, key_selector). source can be any any source that can be queried. key_selector must be an anonymous function that returns a value for each element of source by which the source elements should be grouped.The second variant has the form @groupby(source, key_selector, element_selector). The definition of source and key_selector is the same as in the simple variant. element_selector must be an anonymous function that is applied to each element of the source before that element is placed into a group, i.e. this is a projection function."
+},
+
+{
+    "location": "experimental.html#The-@orderby-command-1",
+    "page": "Experimental Features",
+    "title": "The @orderby command",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
+    "location": "experimental.html#The-..-syntax-1",
+    "page": "Experimental Features",
+    "title": "The .. syntax",
+    "category": "section",
+    "text": "The syntax a..b is translated into map(i->i.b, a) in any query expression. This is especially helpful when computing some reduction of a given column of a grouped table."
+},
+
+{
+    "location": "experimental.html#The-_-syntax-1",
+    "page": "Experimental Features",
+    "title": "The _ syntax",
+    "category": "section",
+    "text": "This syntax only works in the standalone query commands. Instead of writing a full anonymous function, for example @select(i->i.a), one can write @select(_.a), where _ stands for the current element, i.e. has the same role as the argument of the anonymous function."
+},
+
+{
     "location": "internals.html#",
     "page": "Internals",
     "title": "Internals",
