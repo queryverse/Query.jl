40703b61560207e6ae27fdc7bf2be13931492278
David Anthoff <anthoff@berkeley.edu>
2018-04-17 14:32:09 -0700

    Fix IndexedTables tests

--- test/test_indexedtables.jl
+++ test/test_indexedtables.jl
@@ -1,8 +1,7 @@
 using IndexedTables
 
-source_indexedtable = IndexedTable(Columns(city = [fill("New York",3); fill("Boston",3)],
-    date = repmat(Date(2016,7,6):Date(2016,7,8), 2)),
-    Columns(value=[91,89,91,95,83,76]))
+source_indexedtable = table([fill("New York",3); fill("Boston",3)],
+    repmat(Date(2016,7,6):Date(2016,7,8), 2),[91,89,91,95,83,76], names=[:city, :date, :value])
 
 q = @from i in source_indexedtable begin
     @where i.city=="New York"
