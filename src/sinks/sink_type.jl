032b94c4f4a99fa46daeb98f2aef4105fa8cc497
David Anthoff <anthoff@berkeley.edu>
2018-05-14 13:24:10 -0700

    Add a collect method for function sinks

--- src/sinks/sink_type.jl
+++ src/sinks/sink_type.jl
@@ -2,6 +2,10 @@ function collect{T}(enumerable::Enumerable, ::Type{T})
     return T(enumerable)
 end
 
+function collect(enumerable::Enumerable, f::Function)
+    return f(enumerable)
+end
+
 function collect{T, TS,Provider}(source::Queryable{TS,Provider}, ::Type{T})
     collect(QueryOperators.query(collect(source)), T)
 end
