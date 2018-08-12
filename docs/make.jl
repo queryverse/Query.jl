1cdd0ab9866cc6d173fd11c9c79b4bc9900e81c5
David Anthoff <anthoff@berkeley.edu>
2018-08-11 22:00:52 -0700

    Update doc build config

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,7 @@ using Documenter, Query
 
 # Install dependencies and precompile everything
 Pkg.add("DataFrames")
-Pkg.add("TypedTables")
-Pkg.add("DataStreams")
-Pkg.add("CSV")
-Pkg.add("Feather")
 using DataFrames
-using NamedTuples
-using TypedTables
-using DataStreams
-using CSV
-using Feather
 
 makedocs(
 	modules = [Query],
@@ -31,6 +22,6 @@ deploydocs(
     deps = nothing,
     make = nothing,
     target = "build",
-    repo = "github.com/davidanthoff/Query.jl.git",
-    julia = "0.6"
+    repo = "github.com/queryverse/Query.jl.git",
+    julia = "0.7"
 )
