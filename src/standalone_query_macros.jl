b88ac877c2df42fc96c3cebca91f2ed52bc863d7
Brandon Taylor <brandon.taylor221@gmail.com>
2018-09-15 15:13:26 -0400

    templating

--- src/standalone_query_macros.jl
+++ src/standalone_query_macros.jl
@@ -1,4 +1,4 @@
-function standalone_template(afunction, source, args...)
+function standalone_template(afunction, source, args)
     escaped_args = esc.(helper_replace_anon_func_syntax.(args))
 
     args = zip(escaped_args, quot.(escaped_args)) |> flatten
@@ -7,7 +7,7 @@ function standalone_template(afunction, source, args...)
         helper_replace_field_extraction_syntax
 end
 
-function anonymous_template(afunction, args...)
+function anonymous_template(afunction, args)
     escaped_args = esc.(helper_replace_anon_func_syntax.(args))
 
     args = zip(escaped_args, quot.(escaped_args)) |> flatten
@@ -16,7 +16,7 @@ function anonymous_template(afunction, args...)
         helper_replace_field_extraction_syntax
 end
 
-function join_template(afunction, source1, source2, args...)
+function join_template(afunction, source1, source2, args)
     escaped_args = esc.(helper_replace_anon_func_syntax.(args))
     args = zip(escaped_args, quot.(escaped_args)) |> flatten
     :(QueryOperators.$afunction(QueryOperators.query($(esc(source1))), QueryOperators.query($(esc(source2))), $(args...))) |>
@@ -24,7 +24,7 @@ function join_template(afunction, source1, source2, args...)
         helper_replace_field_extraction_syntax
 end
 
-function anonymous_join_template(afunction, source2, args...)
+function anonymous_join_template(afunction, source2, args)
     escaped_args = esc.(helper_replace_anon_func_syntax.(args))
     args = zip(escaped_args, quot.(escaped_args)) |> flatten
     :(i -> QueryOperators.$afunction(QueryOperators.query(i), QueryOperators.query($(esc(source2))), $(args...))) |>
@@ -33,99 +33,99 @@ function anonymous_join_template(afunction, source2, args...)
 end
 
 macro count(source, f)
-    standalone_template(:count, source, f)
+    standalone_template(:count, source, (f,))
 end
 
 macro count(source)
-    standalone_template(:count, source)
+    standalone_template(:count, source, ())
 end
 
 macro count()
-    standalone_template(:count)
+    standalone_template(:count, ())
 end
 
 macro groupby(source, elementSelector, resultSelector)
-    standalone_template(:groupby, source, elementSelector, resultSelector)
+    standalone_template(:groupby, source, (elementSelector, resultSelector))
 end
 
 macro groupby(elementSelector, resultSelector)
-    anonymous_template(:groupby, elementSelector, resultSelector)
+    anonymous_template(:groupby, (elementSelector, resultSelector))
 end
 
 macro groupby(elementSelector)
-    anonymous_template(:groupby, elementSelector, :(i -> i))
+    anonymous_template(:groupby, (elementSelector, :(i -> i)))
 end
 
-macro groupjoin(outer, inner, args...)
-    join_template(:groupjoin, outer, inner, args...)
+macro groupjoin(outer, inner, outerKeySelector, innerKeySelector, resultSelector)
+    join_template(:groupjoin, outer, inner, (outerKeySelector, innerKeySelector, resultSelector))
 end
 
-macro groupjoin(inner, args...)
-    anonymous_join_template(:groupjoin, source2, args...)
+macro groupjoin(inner, outerKeySelector, innerKeySelector, resultSelector)
+    anonymous_join_template(:groupjoin, inner, (outerKeySelector, innerKeySelector, resultSelector))
 end
 
 macro join(outer, inner, outerKeySelector, innerKeySelector, resultSelector)
-    join_template(:join, outer, inner, args...)
+    join_template(:join, outer, inner, (outerKeySelector, innerKeySelector, resultSelector))
 end
 
 macro join(inner, outerKeySelector, innerKeySelector, resultSelector)
-    anonymous_join_template(:join, source2, args...)
+    anonymous_join_template(:join, inner, (outerKeySelector, innerKeySelector, resultSelector))
 end
 
 macro orderby(source, f)
-    standalone_template(:orderby, source, f)
+    standalone_template(:orderby, source, (f,))
 end
 
 macro orderby(f)
-    anonymous_template(:orderby, f)
+    anonymous_template(:orderby, (f,))
 end
 
 macro orderby_descending(source, f)
-    standalone_template(:orderby_descending, source, f)
+    standalone_template(:orderby_descending, source, (f,))
 end
 
 macro orderby_descending(f)
-    anonymous_template(:orderby_descending, f)
+    anonymous_template(:orderby_descending, (f,))
 end
 
 macro thenby(source, f)
-    standalone_template(:thenby, source, f)
+    standalone_template(:thenby, source, (f,))
 end
 
 macro thenby(f)
-    anonymous_template(:thenby, f)
+    anonymous_template(:thenby, (f,))
 end
 
 macro thenby_descending(source, f)
-    standalone_template(:thenby, source, f)
+    standalone_template(:thenby, source, (f,))
 end
 
 macro thenby_descending(f)
-    anonymous_template(:thenby_descending, f)
+    anonymous_template(:thenby_descending, (f,))
 end
 
 macro map(source, f)
-    standalone_template(:map, source, f)
+    standalone_template(:map, source, (f,))
 end
 
 macro map(f)
-    anonymous_template(:map, f)
+    anonymous_template(:map, (f,))
 end
 
 macro mapmany(source, collectionSelector,resultSelector)
-    standalone_template(:mapmany, source, collectionSelector, resultSelector)
+    standalone_template(:mapmany, source, (collectionSelector, resultSelector))
 end
 
 macro mapmany(collectionSelector,resultSelector)
-    anonymous_template(:mapmany, source, collectionSelector, resultSelector)
+    anonymous_template(:mapmany, source, (collectionSelector, resultSelector))
 end
 
 macro filter(source, f)
-    standalone_template(:filter, source, f)
+    standalone_template(:filter, source, (f,))
 end
 
 macro filter(f)
-    standalone_template(:filter, f)
+    standalone_template(:filter, (f,))
 end
 
 macro take(source, n)
