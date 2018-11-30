0faec873c8a0eae17d07624e42faacd86aa3cbbc
Tony Lian <1040424979@qq.com>
2018-11-29 18:34:19 -0800

    Add tests for gather operator

--- test/test_standalone.jl
+++ test/test_standalone.jl
@@ -44,4 +44,12 @@ end
     @test df2[:c] == ["b","c"]
 end
 
+@testset "@gather operator" begin
+
+    @test iterate((Year=[2017,2018,2019], US=[1,2,3], EU=[1,2,3], CN=[1,2,3]) |> @gather) == nothing
+    @test iterate((Year=[2017,2018,2019], US=[1,2,3], EU=[1,2,3], CN=[1,2,3]) |> @gather(:US, :EU, :CN)) == ((key = :US, value = 1, Year = 2017), (Any[(key = :US, value = 1, Year = 2017), (key = :EU, value = 1, Year = 2017), (key = :CN, value = 1, Year = 2017), (key = :US, value = 2, Year = 2018), (key = :EU, value = 2, Year = 2018), (key = :CN, value = 2, Year = 2018), (key = :US, value = 3, Year = 2019), (key = :EU, value = 3, Year = 2019), (key = :CN, value = 3, Year = 2019)], 2))
+    @test eltype((Year=[2017,2018,2019], US=[1,2,3], EU=[1,2,3], CN=[1,2,3]) |> @gather(:US, :EU, :CN)) == NamedTuple{(:key, :value, :Year),Tuple{Symbol,Int64,Int64}}
+
+end
+
 end
