aca98198bc37f687f8f5c6288fb91b752cd31161
David Anthoff <anthoff@berkeley.edu>
2021-04-04 10:30:25 -0700

    Fix doctests

--- test/runtests.jl
+++ test/runtests.jl
@@ -497,7 +497,9 @@ include("test_pipesyntax.jl")
 include("test_macros.jl")
 
 # Only run doctests on 64, all the output checks get messed up with
-# Int32 otherwise
-Int==Int64 && doctest(Query)
+# Int32 otherwise. Also only run on Julia 1.6 and newer, because
+# a lot of output printing was changed and doctests now can't be written
+# to work on multiple Julia versions.
+Int==Int64 && VERSION>=v"1.6" && doctest(Query)
 
 end
